0fbdd4b6637dc9c5d6cf3801dfc9eead3e9ed283
KrisTan2326 <128054270+KrisTan2326@users.noreply.github.com>
2025-10-09 19:23:38 +0530

    Update BasicPlatformController.java

--- BasicPlatformController.java
+++ BasicPlatformController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping("/api/v1/platform")
 @Tag(name = "1. Basic Platform", description = "Basic platform endpoints")
@@ -18,6 +17,7 @@ public class BasicPlatformController {
     }
 
 }
+
 
 
 
